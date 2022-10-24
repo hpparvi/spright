598a8a46653e3a196c38b7e743cc2029858a3b1f
Hannu Parviainen <hpparvi@gmail.com>
2022-10-24 10:02:10 +0100

    Added a new Distribution class.

---
+++ mmmbop/distribution.py
@@ -0,0 +1,96 @@
+from typing import Optional, Callable
+
+import numpy as np
+import arviz as az
+from matplotlib.pyplot import subplots, setp
+from numpy import inf, log, array, percentile, argmin, ndarray
+from scipy.optimize import minimize
+
+from mmmbop.model import spdf
+
+np.seterr(invalid='ignore')
+
+
+class Distribution:
+    def __init__(self, samples: ndarray, quantity: str, modes: tuple[float, Optional[float]], fit: bool = True):
+        self.quantity: str = quantity
+        self.samples: ndarray = samples
+        self.size: int = samples.size
+
+        self.is_bimodal: bool = modes[1] is not None
+
+        self.model: Optional[Callable] = None
+        self.model_pars: Optional[ndarray] = None
+        self._m1: Optional[float] = None
+        self._m2: Optional[float] = None
+
+        if fit:
+            self._fit_distribution(*modes)
+
+    def __repr__(self):
+        p = self.model_pars
+        c = f"{self.quantity.capitalize()} distribution\nsize: {self.size}\nis bimodal: {self.is_bimodal}\n\nDistribution model:\n"
+        if self.is_bimodal:
+            s = f"  {1 - p[0]:3.2f} × T(m={p[1]:3.2f}, σ={p[2]:3.2f}, λ={p[3]:3.2f})\n+ {p[0]:3.2f} × T(m={p[4]:3.2f}, σ={p[5]:3.2f}, λ={p[6]:3.2f})"
+        else:
+            s = f"  T(m={p[0]:3.2f}, σ={p[1]:3.2f}, λ={p[2]:3.2f})"
+        return c + s
+
+    def _fit_kde(self) -> tuple[ndarray, ndarray]:
+        return az.kde(self.samples, adaptive=True)
+
+    def _fit_distribution(self, m1: float, m2: Optional[float]) -> tuple[Callable, ndarray, float, Optional[float]]:
+        if m2 is None:
+            def dmodel(x, pv):
+                return spdf(x, *pv)
+
+            def minfun(pv):
+                if any(pv < 0.0) or pv[2] > 7.0:
+                    return inf
+                return -log(dmodel(self.samples, pv)).sum() / self.size
+
+            self._minimization_result = res = minimize(minfun, array([m1, 0.1, 1.0]), method='powell')
+            return dmodel, res.x, res.x[1], None
+        else:
+            def dmodel(x, pv):
+                return (1 - pv[0]) * spdf(x, *pv[1:4]) + pv[0] * spdf(x, *pv[4:])
+
+            def minfun(pv):
+                if any(pv < 0.0) or pv[0] > 1 or any(pv[[3, 6]] > 7.0) or (pv[1] > pv[4]):
+                    return inf
+                return -log(dmodel(self.samples, pv)).sum() / self.size
+
+            self._minimization_result = res = minimize(minfun, array([0.5, m1, 0.5, 1.5, m2, 0.5, 1.5]),
+                                                       method='powell')
+            self.model, self.model_pars, self._m1, self._m2 = dmodel, res.x, res.x[1], res.x[4]
+            return dmodel, res.x, res.x[1], res.x[4]
+
+    def plot(self, plot_model: bool = True, plot_modes: bool = True, ax = None):
+        ps = percentile(self.samples, [50, 16, 84, 2.5, 97.5])
+        x, y = self._fit_kde()
+        il, iu = argmin(abs(x - ps[1])), argmin(abs(x - ps[2]))
+        ill, iuu = argmin(abs(x - ps[3])), argmin(abs(x - ps[4]))
+
+        if ax is None:
+            fig, ax = subplots()
+        else:
+            fig = None
+
+        ax.fill_between(x, y, alpha=0.5)
+        ax.fill_between(x[ill:iuu], y[ill:iuu], lw=2, fc='k', alpha=0.25)
+        ax.fill_between(x[il:iu], y[il:iu], lw=2, fc='k', alpha=0.25)
+        ax.plot(x, y, 'k')
+        if plot_model:
+            ax.plot(x, self.model(x, self.model_pars), '--k')
+        if plot_modes:
+            ax.axvline(self._m1, c='k', ls='--')
+            if self._m2 is not None:
+                ax.axvline(self._m2, c='k', ls='--')
+
+        xlabel = {'density': r'Density [g cm$^{-3}$]',
+                  'relative density': r'Density [$\rho_{rocky}$]',
+                  'mass': r'Mass [M$_\oplus$]'}[self.quantity]
+
+        setp(ax, ylabel='Posterior probability', xlabel=xlabel, yticks=[], xlim=percentile(self.samples, [1, 99]))
+        if fig is not None:
+            fig.tight_layout()
--- mmmbop/rmrelation.py
+++ mmmbop/rmrelation.py
@@ -1,19 +1,16 @@
 from pathlib import Path
 from typing import Union
 
-import arviz as az
 import astropy.io.fits as pf
 import numpy as np
 from astropy.table import Table
-from matplotlib.pyplot import subplots, setp
-from numpy import arange, inf, percentile, argmin
-from pytransit.utils.de import DiffEvol as DE
-from scipy.optimize import minimize
+from numpy import arange
 
-from mmmbop.model import sample_density, sample_mass, rocky_radius_density
-from mmmbop.model import spdf
+from .distribution import Distribution
+from .model import sample_density, sample_mass, rocky_radius_density
 
 np.seterr(invalid='ignore')
+
 
 class RadiusMassRelation:
     def __init__(self, fname: Union[str, Path] = 'rdmap.fits'):
@@ -27,7 +24,25 @@ class RadiusMassRelation:
             self.probs = h1['CRVAL1'] + arange(h1['NAXIS1']) * h1['CDELT1']
             self.posterior_samples = Table.read(fname).to_pandas()
 
-    def _identify_modes(self, r, x, y, d, x_is_mass: bool = False):
+    def sample(self, quantity, radius, nsamples: int = 5000):
+        try:
+            r, re = radius
+        except TypeError:
+            r, re = radius, 1e-4
+        if quantity == 'density':
+            s = sample_density((r, re), self.radii, self.probs, self.icdf, nsamples)
+        else:
+            s = sample_mass((r, re), self.radii, self.probs, self.icdf, nsamples)
+        return Distribution(s, quantity, self._identify_modes(r, quantity))
+
+    def sample_density(self, r: float, re: float, nsamples: int = 5000):
+        return sample_density((r, re), self.radii, self.probs, self.icdf, nsamples)
+
+    def sample_mass(self, r: float, re: float, nsamples: int = 5000):
+        return sample_mass((r, re), self.radii, self.probs, self.icdf, nsamples)
+
+    def _identify_modes(self, r, quantity: str = 'density'):
+        assert quantity in ('density', 'relative density', 'mass')
         ps = self.posterior_samples
 
         rw_start = ps.rrw1.quantile(0.15)
@@ -35,7 +50,11 @@ class RadiusMassRelation:
         wp_start = ps.rwp1.quantile(0.15)
         wp_end = ps.rwp2.quantile(0.85)
 
-        c = r ** 3 / 5.0 if x_is_mass else 1.0
+        if quantity == 'density':
+            c = 1.0
+        else:
+            c = r ** 3 / 5.0
+
         rd = rocky_radius_density(r)
         rocky_density = c * ps.mrr.median() * rd
         water_density = c * ps.mrw.median() * rd
@@ -52,72 +71,3 @@ class RadiusMassRelation:
         else:
             m1, m2 = puffy_density, None
         return m1, m2
-
-    def _fit_distribution(self, x, y, m1, m2):
-        if m2 is None:
-            def dmodel(x, pv):
-                return pv[0] * spdf(x, *pv[1:])
-
-            def minfun(pv):
-                if any(pv < 0.0) or pv[1] > x[-1]:
-                    return inf
-                return ((y - dmodel(x, pv)) ** 2).sum()
-
-            res = minimize(minfun, [2.0, m1, 0.1, 1.0], method='bfgs')
-            return dmodel, res.x, res.x[1], None
-        else:
-            def dmodel(x, pv):
-                return pv[0] * spdf(x, *pv[1:4]) + pv[4] * spdf(x, *pv[5:])
-
-            def minfun(pv):
-                if any(pv < 0.0) or any(pv[[1, 5]] > x[-1]) or any(pv[[3, 7]] > 7.0) or (pv[1] > pv[5]):
-                    return inf
-                return ((y - dmodel(x, pv)) ** 2).sum()
-
-            de = DE(minfun, bounds=[[0, 10], [x[0], x[-1]], [0.1, 3.0], [0.1, 7.0],
-                                    [0, 10], [x[0], x[-1]], [0.1, 3.0], [0.1, 7.0]],
-                    npop=100)
-            de.optimize(100)
-            res = minimize(minfun, de.minimum_location)
-            return dmodel, res.x, res.x[1], res.x[5]
-
-    def _plot_distribution(self, r, d, ax, plot_model: bool = True, plot_modes: bool = True, x_is_mass: bool = False):
-        ps = percentile(d, [50, 16, 84, 2.5, 97.5])
-        x, y = az.kde(d, adaptive=True)
-        y /= y.max()
-        il, iu = argmin(abs(x - ps[1])), argmin(abs(x - ps[2]))
-        ill, iuu = argmin(abs(x - ps[3])), argmin(abs(x - ps[4]))
-        ax.fill_between(x, y, alpha=0.5)
-        ax.fill_between(x[ill:iuu], y[ill:iuu], lw=2, fc='k', alpha=0.25)
-        ax.fill_between(x[il:iu], y[il:iu], lw=2, fc='k', alpha=0.25)
-        ax.plot(x, y, 'k')
-        if plot_model or plot_modes:
-            m1, m2 = self._identify_modes(r, x, y, d, x_is_mass)
-            model, pv, m1, m2 = self._fit_distribution(x, y, m1, m2)
-            if plot_model:
-                ax.plot(x, model(x, pv), '--k')
-            if plot_modes:
-                ax.axvline(m1, c='k', ls='--')
-                if m2 is not None:
-                    ax.axvline(m2, c='k', ls='--')
-
-    def plot_density(self, r, re, relative_to_rocky: bool = False):
-        density = sample_density((r, re), self.radii, self.probs, self.icdf)
-        if relative_to_rocky:
-            density /= rocky_radius_density(r)
-        fig, ax = subplots()
-        self._plot_distribution(r, density, ax)
-        xlabel = r'Density [$\rho_{rocky}$]' if relative_to_rocky else 'Density [g cm$^{-3}$]'
-        setp(ax, xlabel=xlabel, ylabel='Posterior probability', yticks=[])
-        return density
-
-    def plot_mass(self, r: float, re: float):
-        mass = sample_mass((r, re), self.radii, self.probs, self.icdf)
-        fig, ax = subplots()
-        self._plot_distribution(r, mass, ax, x_is_mass=True)
-        setp(ax, xlabel='Mass [M$_\oplus$]', ylabel='Posterior probability', yticks=[])
-
-
-rmr = RadiusMassRelation()
-d = rmr.plot_density(1.8, 0.1, False)
-# rmr.plot_mass(1.6, 0.1)
