d7c59c37143f7173e3c8aaf0c01deba09e2e5be2
Hannu Parviainen <hannu@iac.es>
2022-12-10 19:46:56 +0100

    Minor fixes.

--- moot/__init__.py
+++ moot/__init__.py
@@ -1,8 +1,4 @@
-from importlib.metadata import version
-
 from .rmrelation import RMRelation
 from .rmestimator import RMEstimator
 
-version = version('moot')
-
-__all__ = ['version', 'RMRelation', 'RMEstimator']
+__all__ = ['RMRelation', 'RMEstimator']
