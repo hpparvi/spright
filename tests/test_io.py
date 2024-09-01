c3e3fda1b35f9b680b85290a7f9540272efb1259
Hannu Parviainen <hpparvi@gmail.com>
2024-09-01 14:26:34 +0100

    Add test for reading exoplanet_eu CSV file
    Imported `read_exoplanet_eu` function in `tests/test_io.py`. Added a new test, `test_read_exoeu`, to verify correct reading of `exoplanet_eu.csv`. Updated `TEPCat` test to use a simplified file name.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from spright.io import read_stpm, read_tepcat
+from spright.io import read_stpm, read_tepcat, read_exoplanet_eu
 
 root = Path(__file__).parent
 
@@ -7,4 +7,7 @@ def test_read_stmp():
     read_stpm(root / '../spright/data/stpm_230202.csv')
 
 def test_read_tepcat():
-    read_tepcat(root / '../spright/data/TEPCat_FGK_20230522.csv')
+    read_tepcat(root / '../spright/data/TEPCat.csv')
+
+def test_read_exoeu():
+    read_exoplanet_eu(root / '../spright/data/exoplanet_eu.csv')
